c04ab3ad0de64d67f28138e0598b26ffb7be57fd
mawoka37 <47260501-mawoka37@users.noreply.replit.com>
2025-10-17 18:18:50 +0000

    Restored to 'd48c350e071fef5c53561248e8c8df54219a0904'
    Replit-Restored-To: d48c350e071fef5c53561248e8c8df54219a0904

--- models.py
+++ models.py
@@ -176,7 +176,6 @@ class ChapitreComplet(Base):
     
     created_by = Column(String(100), ForeignKey("professeurs.username"), nullable=False)
     created_at = Column(DateTime, default=datetime.utcnow)
-    verrouille = Column(Boolean, default=False, nullable=False)
     
     # Relations
     filiere = relationship("Filiere", back_populates="chapitres")
